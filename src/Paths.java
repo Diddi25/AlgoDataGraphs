2ba76a8106f6fd92f21984e43536c70f4b737567
Diddi <diddi.dutta@outlook.com>
2023-10-27 19:18:10 +0200

    förhoppningsvis klar men fortfarande inte rekursivt

--- src/Paths.java
+++ src/Paths.java
@@ -1,17 +1,22 @@
 public class Paths {
     private City[] path;
     private int traverseIndex;
+    private Integer dynamicMaxValue;
     public Paths() {
         path = new City[54];
         traverseIndex = 0;
+        dynamicMaxValue = null;
     }
     private Integer shortest(City fromCity, City toCity) {
-        Integer shortestPath = null;
+        Integer shortestPath = 0;
         Integer smallestDistanceCandidate;
         while (!fromCity.equals(toCity)) {
             smallestDistanceCandidate = 850;
             for (Connection candidate: fromCity.connections) {
-                if (candidate.distanceInMinutes < smallestDistanceCandidate) {
+                if (candidate.city.equals(toCity)) {
+                    dynamicMaxValue = candidate.distanceInMinutes;
+                }
+                if (candidate.distanceInMinutes < smallestDistanceCandidate && shortestPath < dynamicMaxValue) {
                     smallestDistanceCandidate = candidate.distanceInMinutes;
                     if (testIfCityIsAlreadyInPathRoute(candidate.city)) {
                         path[traverseIndex++] = candidate.city;
